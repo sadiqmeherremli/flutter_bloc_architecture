3a05721f5090a93e35f98f54a4d8fb4b5f0cabe8
sadiqmeherremli <sadiqmeherremli.dev@gmail.com>
2024-12-26 10:54:51 +0400

    commit

---
+++ flutter_bloc_pattern/lib/core/theme/lightTheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LightTheme {
+  final theme = ThemeData(
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: Colors.amber
+    ),
+    
+   
+  );
+}
